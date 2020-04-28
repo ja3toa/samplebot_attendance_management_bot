2a2382675bbc557ffd67c30d15c87f671cb4c88b
ja3toa <64389369+ja3toa@users.noreply.github.com>
2020-04-28 13:20:09 +0900

    Update config.py

--- conf/config.py
+++ conf/config.py
@@ -25,15 +25,15 @@ DEFAULT_LANG = 'ja'
 TZone = "Asia/Tokyo"
 
 # account
-ADMIN_ACCOUNT = "admin@xxx.com"
-DOMAIN_ID = 0
+ADMIN_ACCOUNT = "admin@hasebuilding"
+DOMAIN_ID = 10049225
 
 # api
-API_ID = "kr1EHxxxxfJVz"
-SERVER_CONSUMER_KEY = "To8SnC7sxxxxxjqXZhO"
+API_ID = "jp1BzoVTxMIMd"
+SERVER_CONSUMER_KEY = "LjeV_1TCQZnvd1IM1S9r"
 
-SERVER_ID = "96460cc1e778402xxxxxfe35fa97ce76"
-SECRET_KEY_NAME = "private_201xxxxx164308.key"
+SERVER_ID = "43e3cc391d5148c589299c7ab25fcd81"
+SECRET_KEY_NAME = "private_20200428131320.key"
 
 # LOCAL ADDRESS
 LOCAL_ADDRESS = "https://hase-botan02.herokuapp.com/"
